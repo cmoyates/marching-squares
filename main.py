26fbf934283e5f3e21fc1043362ca15207fb1fb0
cmoyates <cmoyates@gmail.com>
2024-12-30 01:47:08 -0330

    Simple marching squares setup

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-# Example file showing a basic pygame "game loop"
 from perlin_noise import PerlinNoise
 import pygame
-from math import ceil
+from math import floor
 
 WIDTH = 640
 HEIGHT = 360
 DOT_SPACING = 20
+
+MARCHING_SQUARES_LINES: list[list[float]] = [
+    [],
+    [[0, 0.5], [0.5, 1]],
+    [[0.5, 1], [1, 0.5]],
+    [[0, 0.5], [1, 0.5]],
+    [[0.5, 0], [1, 0.5]],
+    [[0, 0.5], [0.5, 0], [0.5, 1], [1, 0.5]],
+    [[0.5, 0], [0.5, 1]],
+    [[0, 0.5], [0.5, 0]],
+    [[0, 0.5], [0.5, 0]],
+    [[0.5, 0], [0.5, 1]],
+    [[0, 0.5], [0.5, 1], [0.5, 0], [1, 0.5]],
+    [[0.5, 0], [1, 0.5]],
+    [[0, 0.5], [1, 0.5]],
+    [[0.5, 1], [1, 0.5]],
+    [[0, 0.5], [0.5, 1]],
+    [],
+]
 
 # pygame setup
 pygame.init()
@@ -14,6 +32,15 @@ clock = pygame.time.Clock()
 running = True
 
 noise = PerlinNoise(octaves=3.5, seed=777)
+
+sampled_noise = []
+
+for x in range(0, WIDTH + DOT_SPACING, DOT_SPACING):
+    sampled_noise_row = []
+    for y in range(0, HEIGHT + DOT_SPACING, DOT_SPACING):
+        n = noise([x / WIDTH, y / HEIGHT])
+        sampled_noise_row.append(floor(n + 1))
+    sampled_noise.append(sampled_noise_row)
 
 while running:
     # poll for events
@@ -27,11 +54,40 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("grey")
 
-    for x in range(0, WIDTH + DOT_SPACING, DOT_SPACING):
-        for y in range(0, HEIGHT + DOT_SPACING, DOT_SPACING):
-            n = noise([x / WIDTH, y / HEIGHT])
-            brightness = ceil(n) * 255
-            pygame.draw.circle(screen, (brightness, brightness, brightness), (x, y), 5)
+    for x in range(WIDTH // DOT_SPACING + 1):
+        for y in range(HEIGHT // DOT_SPACING + 1):
+            noise_at_point = sampled_noise[x][y]
+            brightness = noise_at_point * 255
+            pygame.draw.circle(
+                screen,
+                (brightness, brightness, brightness),
+                (x * DOT_SPACING, y * DOT_SPACING),
+                5,
+            )
+
+    for x in range(WIDTH // DOT_SPACING):
+        for y in range(HEIGHT // DOT_SPACING):
+            noise_top_left = sampled_noise[x][y]
+            noise_top_right = sampled_noise[x + 1][y]
+            noise_bottom_left = sampled_noise[x][y + 1]
+            noise_bottom_right = sampled_noise[x + 1][y + 1]
+
+            lines_index = (
+                noise_top_left * 8
+                + noise_top_right * 4
+                + noise_bottom_right * 2
+                + noise_bottom_left
+            )
+
+            lines_points: list[float] = MARCHING_SQUARES_LINES[lines_index]
+            for i in range(0, len(lines_points), 2):
+                start = lines_points[i]
+                end = lines_points[i + 1]
+                start_x = (x + start[0]) * DOT_SPACING
+                start_y = (y + start[1]) * DOT_SPACING
+                end_x = (x + end[0]) * DOT_SPACING
+                end_y = (y + end[1]) * DOT_SPACING
+                pygame.draw.line(screen, "black", (start_x, start_y), (end_x, end_y))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
