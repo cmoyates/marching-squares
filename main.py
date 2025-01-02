124279a866ad01caffabd50bac7dfabbb53d69df
cmoyates <cmoyates@gmail.com>
2025-01-02 12:31:20 -0330

    Some visual cleanup

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from time import time
 
 WIDTH = 640
 HEIGHT = 360
+
 DOT_SPACING = 20
 DOT_RADIUS = 5
-NOISE_STEPS = 2
+
+NOISE_STEPS = 3
 
 MARCHING_SQUARES_LINES: list[list[float]] = [
     [],
@@ -63,6 +65,7 @@ def save_image():
         for y in range(HEIGHT // DOT_SPACING + 1):
             noise_at_point = sampled_noise[x][y]
             brightness = int(noise_at_point * 255 / (NOISE_STEPS - 1))
+            brightness = min(max(brightness, 0), 255)
             color = (brightness, brightness, brightness)
 
             image.paste(
@@ -99,13 +102,33 @@ def main():
 
     global sampled_noise, holding_left_mouse, holding_cmd, mouse_positon
 
-    # Generate the noise
+    # Generate and sample the noise
+
+    min_sample = None
+    max_sample = None
+
     for x in range(0, WIDTH + DOT_SPACING, DOT_SPACING):
         sampled_noise_row = []
         for y in range(0, HEIGHT + DOT_SPACING, DOT_SPACING):
-            n = noise([x / WIDTH, y / HEIGHT])
-            sampled_noise_row.append(floor((NOISE_STEPS) * ((n + 1) / 2)))
+            sample = noise([x / WIDTH, y / HEIGHT])
+            sample_0_to_1 = (sample + 1) / 2
+
+            if min_sample is None or sample_0_to_1 < min_sample:
+                min_sample = sample_0_to_1
+            if max_sample is None or sample_0_to_1 > max_sample:
+                max_sample = sample_0_to_1
+
+            sampled_noise_row.append(sample_0_to_1)
         sampled_noise.append(sampled_noise_row)
+
+    # Normalize the sampled noise
+    for x in range(WIDTH // DOT_SPACING + 1):
+        for y in range(HEIGHT // DOT_SPACING + 1):
+            sampled_noise[x][y] = floor(
+                (sampled_noise[x][y] - min_sample)
+                * (NOISE_STEPS)
+                / (max_sample - min_sample)
+            )
 
     # Populate the dots
     for x in range(WIDTH // DOT_SPACING + 1):
@@ -154,21 +177,39 @@ def main():
         # Reset the screen
         screen.fill("grey")
 
+        for x in range(WIDTH // DOT_SPACING + 1):
+            for y in range(HEIGHT // DOT_SPACING + 1):
+                noise_at_point = sampled_noise[x][y]
+                brightness = int(noise_at_point * 255 / (NOISE_STEPS - 1))
+                brightness = min(max(brightness, 0), 255) / 2
+                color = (brightness, brightness, brightness)
+
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    (
+                        (x - 0.5) * DOT_SPACING,
+                        (y - 0.5) * DOT_SPACING,
+                        DOT_SPACING,
+                        DOT_SPACING,
+                    ),
+                )
+
         # Draw the lines
         for noise_step in range(NOISE_STEPS - 1):
             for x in range(WIDTH // DOT_SPACING):
                 for y in range(HEIGHT // DOT_SPACING):
                     noise_top_left = ceil(
-                        (sampled_noise[x][y] - noise_step) / (NOISE_STEPS - 1)
+                        (sampled_noise[x][y] - noise_step) / (NOISE_STEPS)
                     )
                     noise_top_right = ceil(
-                        (sampled_noise[x + 1][y] - noise_step) / (NOISE_STEPS - 1)
+                        (sampled_noise[x + 1][y] - noise_step) / (NOISE_STEPS)
                     )
                     noise_bottom_right = ceil(
-                        (sampled_noise[x + 1][y + 1] - noise_step) / (NOISE_STEPS - 1)
+                        (sampled_noise[x + 1][y + 1] - noise_step) / (NOISE_STEPS)
                     )
                     noise_bottom_left = ceil(
-                        (sampled_noise[x][y + 1] - noise_step) / (NOISE_STEPS - 1)
+                        (sampled_noise[x][y + 1] - noise_step) / (NOISE_STEPS)
                     )
 
                     lines_index = (
@@ -187,13 +228,13 @@ def main():
                         end_x = (x + end[0]) * DOT_SPACING
                         end_y = (y + end[1]) * DOT_SPACING
                         pygame.draw.line(
-                            screen, "black", (start_x, start_y), (end_x, end_y)
+                            screen, "white", (start_x, start_y), (end_x, end_y)
                         )
 
         # Select dots that are close enough to the mouse
         if holding_left_mouse:
-            for x in range(WIDTH // DOT_SPACING):
-                for y in range(HEIGHT // DOT_SPACING):
+            for x in range(WIDTH // DOT_SPACING + 1):
+                for y in range(HEIGHT // DOT_SPACING + 1):
                     dot_position = (x * DOT_SPACING, y * DOT_SPACING)
                     dot_to_mouse_distance = (
                         mouse_positon[0] - dot_position[0]
